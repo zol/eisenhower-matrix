c9051b9112fb2f9b920cec8331b7cf2f0b10674e
Zoltan Olah <zol@me.com>
2010-02-15 13:09:25 -0800

    Added some basic styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # returns 'current' if the current controller/view match those passed in
+  def current(c, a)
+    (controller.controller_name == c and 
+      controller.action_name == a) ? 'current' : ''
+  end
 end
