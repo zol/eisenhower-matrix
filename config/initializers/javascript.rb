50ca6d6c6f41e6f14e7de1b8efcca8e716ffb459
Zoltan Olah <zol@me.com>
2010-02-15 18:21:09 -0800

    Integrated jquery ui and task drawing.

--- config/initializers/javascript.rb
+++ config/initializers/javascript.rb
@@ -1,4 +1,5 @@
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion(
   :defaults => ['lib/jquery-1.4.1.min', 
                 'lib/jquery.string.1.0',
+                'lib/jquery-ui-1.7.2.custom.min',
                 'framework'])
--- public/javascripts/views/tasks/index.js
+++ public/javascripts/views/tasks/index.js
@@ -1,7 +1,25 @@
 (function($){
   tasksIndex = {
-    onReady: function() {
-      alert('framework is working!');
+    onReady: function() {          
+      $(window).resize(function(){tasksIndex.updateQuadrantHeights();});
+      this.updateQuadrantHeights();
+      
+      $(".sortable").sortable({
+        connectWith: '.sortable',
+        cursor: 'crosshair'
+      }).disableSelection();
+    },
+    updateQuadrantHeights: function() {
+      var windowHeight = $(window).height();
+      var headerHeight = $('#header').outerHeight();
+      var footerHeight = $('#footer').outerHeight();
+      
+      var contentHeight = windowHeight - headerHeight - footerHeight;
+      var quadrantHeight = contentHeight / 2;
+
+      $('#content').height(contentHeight);
+      $('.quadrant').height(quadrantHeight);
+      $('.quadrant ol').height(quadrantHeight); //also size the lists
     }
   }
 }(jQuery));
