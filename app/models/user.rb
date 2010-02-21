e2d1a06bf0d418868f7f63c3ca5c0bb01d8c1cce
Zoltan Olah <zol@me.com>
2010-02-21 00:49:46 -0800

    Added a demo task.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,14 @@ class User < ActiveRecord::Base
   validates_length_of :invitation_code, :is => 8
   validate :correct_invitation_code, :on => :create
   
+  after_create :create_initial_task
+  
 protected
+
+  def create_initial_task
+    self.tasks.create(:title => 'Hi, I\'m a task. Click the checkbox to remove me. ' + 
+    ' You can pick me up and drag me around too!')
+  end
   
   def correct_invitation_code
     # add all numbers together and check if they equal 27 :) (e.g 7f91j28 or 99900000)
